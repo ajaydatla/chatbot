465e9618fc05c5806febd7b6f54859c07483ed23
ajaydatla <ajaydatlalbn06@gmail.com>
2018-07-25 14:57:38 +0530

    send a message added

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,39 @@ class FriendlychatApp extends StatelessWidget{
     );
   }
 }
-class ChatScreen extends StatelessWidget{
+class ChatScreen extends StatefulWidget{
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen>{
+
+  final TextEditingController _textController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+
+     return Scaffold(
       appBar: AppBar(
         title: Text("FriendlyChat"),
       ),
+       body: _buildTextComposer(),
     );
   }
+
+  Widget _buildTextComposer(){
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
 }
+
